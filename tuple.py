1de4835fd8a3016fa72a58196cf352d0fa506cfb
Dhruv Desai <dhruvdesai222@gmail.com>
2023-02-09 09:56:57 +0530

    About Tuple

---
+++ tuple.py
@@ -0,0 +1,8 @@
+# you could not insert, remove like that in tuple (Compare to list)
+# only 2 methods are available (count & index) to use nothing else
+
+my_tuple = ("Dhruv", "Desai", "OM", "Jenish")
+print(my_tuple)
+
+print(my_tuple.count("Dhruv"))
+print(my_tuple.index("OM"))
